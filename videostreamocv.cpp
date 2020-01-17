03a969d8c8b83e41090879c59bd2067c601e1533
Daniel Aharoni <DBAharoni@gmail.com>
2020-01-16 21:28:14 -0800

    Move all initialization commands from DAQ firmware to DAQ software.

--- videostreamocv.cpp
+++ videostreamocv.cpp
@@ -102,16 +102,18 @@ void VideoStreamOCV::startStream()
 //                            heading = static_cast<qint16>(cam->get(cv::CAP_PROP_SATURATION))/16.0;
 //                            roll = static_cast<qint16>(cam->get(cv::CAP_PROP_HUE))/16.0;
 //                            pitch = static_cast<qint16>(cam->get(cv::CAP_PROP_GAIN))/16.0;
+
+                            // BNO output is a unit quaternion after 2^14 division
                             w = static_cast<qint16>(cam->get(cv::CAP_PROP_SATURATION))/16384.0;
                             x = static_cast<qint16>(cam->get(cv::CAP_PROP_HUE))/16384.0;
                             y = static_cast<qint16>(cam->get(cv::CAP_PROP_GAIN))/16384.0;
                             z = static_cast<qint16>(cam->get(cv::CAP_PROP_BRIGHTNESS))/16384.0;
-                            norm = sqrt(w*w + x*x + y*y + z*z);
+//                            norm = sqrt(w*w + x*x + y*y + z*z);
                             bnoBuffer[(idx%frameBufferSize)*4 + 0] = w;
                             bnoBuffer[(idx%frameBufferSize)*4 + 1] = x;
                             bnoBuffer[(idx%frameBufferSize)*4 + 2] = y;
                             bnoBuffer[(idx%frameBufferSize)*4 + 3] = z;
-                            qDebug() << norm << w << x << y << z;
+//                            qDebug() << norm << w << x << y << z;
                         }
                         if (daqFrameNum != nullptr) {
                             *daqFrameNum = cam->get(cv::CAP_PROP_CONTRAST) - daqFrameNumOffset;
