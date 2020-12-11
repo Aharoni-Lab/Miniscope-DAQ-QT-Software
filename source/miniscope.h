91f37b37631a408cada7282732fd4e0ae6b71683
Daniel Aharoni <dbaharoni@gmail.com>
2020-12-10 21:25:32 -0800

    Move miniscope class into videodevice and give miniscope class videodevice inheritance
    Seems like this works.
    
    TODO:
    * inlcude only common functions in the videodevice class that are shared between miniscopes and behavior cameras.
    * give behaviorcam class videodevice inheritance

--- source/miniscope.h
+++ source/miniscope.h
@@ -16,6 +16,8 @@
 #include "videostreamocv.h"
 #include "videodisplay.h"
 #include "newquickview.h"
+#include "videodevice.h"
+
 #include <opencv2/opencv.hpp>
 
 
@@ -34,106 +36,11 @@
 #define BASELINE_FRAME_BUFFER_SIZE  128
 
 
-class Miniscope : public QObject
+class Miniscope : public VideoDevice
 {
-    Q_OBJECT
+
 public:
     explicit Miniscope(QObject *parent = nullptr, QJsonObject ucMiniscope = QJsonObject());
-    void createView();
-    void connectSnS();
-    void defineDeviceAddrs();
-    void parseUserConfigMiniscope();
-    void sendInitCommands();
-    QString getCompressionType();
-    cv::Mat* getFrameBufferPointer(){return frameBuffer;}
-    qint64* getTimeStampBufferPointer(){return timeStampBuffer;}
-    float* getBNOBufferPointer() { return bnoBuffer; }
-    int getBufferSize() {return FRAME_BUFFER_SIZE;}
-    QSemaphore* getFreeFramesPointer(){return freeFrames;}
-    QSemaphore* getUsedFramesPointer(){return usedFrames;}
-    QAtomicInt* getAcqFrameNumPointer(){return m_acqFrameNum;}
-    QAtomicInt* getDAQFrameNumPointer() { return m_daqFrameNum; }
-    QString getDeviceName(){return m_deviceName;}
-    bool getHeadOrienataionStreamState() { return m_headOrientationStreamState;}
-    bool getHeadOrienataionFilterState() { return m_headOrientationFilterState;}
-
-signals:
-    // TODO: setup signals to configure camera in thread
-    void setPropertyI2C(long preambleKey, QVector<quint8> packet);
-    void onPropertyChanged(QString devieName, QString propName, QVariant propValue);
-    void sendMessage(QString msg);
-    void takeScreenShot(QString type);
-    void setExtTriggerTrackingState(bool state);
-    void extTriggered(bool state);
-    void startRecording();
-    void stopRecording();
-
-public slots:
-    void sendNewFrame();
-    void testSlot(QString, double);
-    void handlePropChangedSignal(QString type, double displayValue, double i2cValue, double i2cValue2);
-    void handleTakeScreenShotSignal();
-    void handleDFFSwitchChange(bool checked);
-    void handleSaturationSwitchChanged(bool checked);
-    void handleSetExtTriggerTrackingState(bool state);
-    void handleRecordStart(); // Currently used to toggle LED on and off
-    void handleRecordStop(); // Currently used to toggle LED on and off
-    void handleInitCommandsRequest();
-    void close();
-
-private:
-    void getMiniscopeConfig(QString deviceType);
-    void configureMiniscopeControls();
-    QVector<QMap<QString, int>> parseSendCommand(QJsonArray sendCommand);
-    int processString2Int(QString s);
-    QMap<QString,quint16> deviceAddr;
-
-    int m_camConnected;
-    NewQuickView *view;
-    VideoStreamOCV *miniscopeStream;
-    QThread *videoStreamThread;
-    cv::Mat frameBuffer[FRAME_BUFFER_SIZE];
-    cv::Mat tempFrame;
-    qint64 timeStampBuffer[FRAME_BUFFER_SIZE];
-//    float bnoBuffer[FRAME_BUFFER_SIZE*3];
-    float bnoBuffer[FRAME_BUFFER_SIZE*5]; //w,x,y,z,norm
-    QSemaphore *freeFrames;
-    QSemaphore *usedFrames;
-    QObject *rootObject;
-    VideoDisplay *vidDisplay;
-    QQuickItem *bnoDisplay;
-    QTimer *timer;
-//    QImage testImage;
-    int m_previousDisplayFrameNum;
-    QAtomicInt *m_acqFrameNum;
-    QAtomicInt *m_daqFrameNum;
-
-//    QAtomicInt *m_DAQTimeStamp;
-
-    // User Config parameters
-    QJsonObject m_ucMiniscope;
-    int m_deviceID;
-    QString m_deviceName;
-    QString m_deviceType;
-//    QMap<QString,double> m_ucParameters; // holds all number parameters
-
-
-    QJsonObject m_cMiniscopes; // Consider renaming to not confuse with ucMiniscopes
-    QMap<QString,QVector<QMap<QString, int>>> m_controlSendCommand;
-    QMap<QString, int> m_sendCommand;
-
-    bool m_headOrientationStreamState;
-    bool m_headOrientationFilterState;
-    QString m_compressionType;
-    QString m_displatState;
-
-    cv::Mat baselineFrameBuffer[BASELINE_FRAME_BUFFER_SIZE];
-    cv::Mat baselineFrame;
-    int baselineFrameBufWritePos;
-    qint64 baselinePreviousTimeStamp;
-
-    double m_lastLED0Value;
-    bool m_extTriggerTrackingState;
 };
 
 
--- source/videodevice.h
+++ source/videodevice.h
@@ -1,11 +1,144 @@
 #ifndef VIDEODEVICE_H
 #define VIDEODEVICE_H
 
+// This is the base class for all video type devices that stream data into this software
 
-class VideoDevice
+#include <QObject>
+#include <QThread>
+#include <QSemaphore>
+#include <QTimer>
+#include <QAtomicInt>
+#include <QJsonObject>
+#include <QQuickView>
+#include <QMap>
+#include <QVector>
+#include <QQuickItem>
+#include <QVariant>
+
+#include "videostreamocv.h"
+#include "videodisplay.h"
+#include "newquickview.h"
+#include <opencv2/opencv.hpp>
+
+// ------- Defines for UVC to I2C communication ----------
+// Will be replaced once Miniscope communication is moved to vendor class USB
+#define PROTOCOL_I2C            -2
+#define PROTOCOL_SPI            -3
+#define SEND_COMMAND_VALUE_H    -5
+#define SEND_COMMAND_VALUE_L    -6
+#define SEND_COMMAND_VALUE      -6
+#define SEND_COMMAND_VALUE_H16  -7
+#define SEND_COMMAND_VALUE_H24  -8
+#define SEND_COMMAND_VALUE2_H   -9
+#define SEND_COMMAND_VALUE2_L   -10
+#define SEND_COMMAND_ERROR      -20
+// ---------------------------------------------------------
+
+// ----- Size of frame buffer in RAM for each device -------
+#define FRAME_BUFFER_SIZE   128
+#define BASELINE_FRAME_BUFFER_SIZE  128
+// ---------------------------------------------------------
+
+class VideoDevice : public QObject
 {
+    Q_OBJECT
 public:
-    VideoDevice();
+    explicit VideoDevice(QObject *parent = nullptr, QJsonObject ucDevice = QJsonObject());
+    void createView();
+    void connectSnS();
+    void defineDeviceAddrs();
+    void parseUserConfigDevice();
+    void sendInitCommands();
+    QString getCompressionType();
+    cv::Mat* getFrameBufferPointer(){return frameBuffer;}
+    qint64* getTimeStampBufferPointer(){return timeStampBuffer;}
+    float* getBNOBufferPointer() { return bnoBuffer; }
+    int getBufferSize() {return FRAME_BUFFER_SIZE;}
+    QSemaphore* getFreeFramesPointer(){return freeFrames;}
+    QSemaphore* getUsedFramesPointer(){return usedFrames;}
+    QAtomicInt* getAcqFrameNumPointer(){return m_acqFrameNum;}
+    QAtomicInt* getDAQFrameNumPointer() { return m_daqFrameNum; }
+    QString getDeviceName(){return m_deviceName;}
+    bool getHeadOrienataionStreamState() { return m_headOrientationStreamState;}
+    bool getHeadOrienataionFilterState() { return m_headOrientationFilterState;}
+
+signals:
+    // TODO: setup signals to configure camera in thread
+    void setPropertyI2C(long preambleKey, QVector<quint8> packet);
+    void onPropertyChanged(QString devieName, QString propName, QVariant propValue);
+    void sendMessage(QString msg);
+    void takeScreenShot(QString type);
+    void setExtTriggerTrackingState(bool state);
+    void extTriggered(bool state);
+    void startRecording();
+    void stopRecording();
+
+public slots:
+    void sendNewFrame();
+    void testSlot(QString, double);
+    void handlePropChangedSignal(QString type, double displayValue, double i2cValue, double i2cValue2);
+    void handleTakeScreenShotSignal();
+    void handleDFFSwitchChange(bool checked);
+    void handleSaturationSwitchChanged(bool checked);
+    void handleSetExtTriggerTrackingState(bool state);
+    void handleRecordStart(); // Currently used to toggle LED on and off
+    void handleRecordStop(); // Currently used to toggle LED on and off
+    void handleInitCommandsRequest();
+    void close();
+
+private:
+    void getDeviceConfig(QString deviceType);
+    void configureDeviceControls();
+    QVector<QMap<QString, int>> parseSendCommand(QJsonArray sendCommand);
+    int processString2Int(QString s);
+    QMap<QString,quint16> deviceAddr;
+
+    int m_camConnected;
+    NewQuickView *view;
+    VideoStreamOCV *deviceStream;
+    QThread *videoStreamThread;
+    cv::Mat frameBuffer[FRAME_BUFFER_SIZE];
+    cv::Mat tempFrame;
+    qint64 timeStampBuffer[FRAME_BUFFER_SIZE];
+//    float bnoBuffer[FRAME_BUFFER_SIZE*3];
+    float bnoBuffer[FRAME_BUFFER_SIZE*5]; //w,x,y,z,norm
+    QSemaphore *freeFrames;
+    QSemaphore *usedFrames;
+    QObject *rootObject;
+    VideoDisplay *vidDisplay;
+    QQuickItem *bnoDisplay;
+    QTimer *timer;
+//    QImage testImage;
+    int m_previousDisplayFrameNum;
+    QAtomicInt *m_acqFrameNum;
+    QAtomicInt *m_daqFrameNum;
+
+//    QAtomicInt *m_DAQTimeStamp;
+
+    // User Config parameters
+    QJsonObject m_ucDevice;
+    int m_deviceID;
+    QString m_deviceName;
+    QString m_deviceType;
+//    QMap<QString,double> m_ucParameters; // holds all number parameters
+
+
+    QJsonObject m_cDevice; // Consider renaming to not confuse with ucMiniscopes
+    QMap<QString,QVector<QMap<QString, int>>> m_controlSendCommand;
+    QMap<QString, int> m_sendCommand;
+
+    bool m_headOrientationStreamState;
+    bool m_headOrientationFilterState;
+    QString m_compressionType;
+    QString m_displatState;
+
+    cv::Mat baselineFrameBuffer[BASELINE_FRAME_BUFFER_SIZE];
+    cv::Mat baselineFrame;
+    int baselineFrameBufWritePos;
+    qint64 baselinePreviousTimeStamp;
+
+    double m_lastLED0Value;
+    bool m_extTriggerTrackingState;
 };
 
 #endif // VIDEODEVICE_H
